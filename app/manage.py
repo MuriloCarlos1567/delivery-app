0be843fbc8b9bdb64eafb6a32b78a894002d6552
MuriloCarlos1567 <efgs96@gmail.com>
2021-12-10 15:42:41 -0300

    no comments

--- app/manage.py
+++ app/manage.py
@@ -2,8 +2,9 @@ from flask_migrate import upgrade
 from .models import Role
 
 def manage_database():
-    if Role.query.filter_by(id=1).first():
-        upgrade()
-        Role.insert_roles()
-    else:
+    try:
+        if Role.query.filter_by(id=1).first():
+            upgrade()
+            Role.insert_roles()
+    except:
         print('Alredy up')
